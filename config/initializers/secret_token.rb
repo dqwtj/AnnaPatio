7604fffcba15f9305bfa4edcdb804ad543a857f0
Tianju <wtshootj@Tianju-Wangs-MacBook-Air.local>
2013-10-18 20:50:41 +0800

    rails init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnnaPatio::Application.config.secret_key_base = 'fdd33c9da67f69c5202c75f55f8bc23839252b8f3243b64d9a8a0d444e1e5840f5fb69e5c264b0273958047bee99978172fd92e4db7d6cc394861aa6eb8eeb60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnnaPatio::Application.config.session_store :cookie_store, key: '_AnnaPatio_session'
