5c4168d351c6733c8c5e8e9245fee82b03002d4a
Tianju <wtshootj@Tianju-Wangs-MacBook-Air.local>
2013-11-29 01:23:23 +0800

    style fix more

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -9,7 +9,7 @@ class PoemsController < ApplicationController
   end
   
   def index
-    @poems = Poem.desc(:written_at).paginate :page => params[:page], :per_page => 8
+    @poems = Poem.desc(:created_at).paginate :page => params[:page], :per_page => 7
   end
   
   def show
