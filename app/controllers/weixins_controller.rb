0e9196646a4e957d85cbb89dfa2b38d02f0027a6
Tianju <wtshootj@Tianju-Wangs-MacBook-Air.local>
2013-12-25 00:29:50 +0800

    do more to complete test

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,15 +1,13 @@
 # -*- encoding : utf-8 -*-
 class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  #before_filter :check_weixin_legality
+  before_filter :check_weixin_legality
 
   def show
     render :text => params[:echostr]
   end
 
   def create
-    if params[:xml][:MsgType] == "text"
-    end
     if (params[:xml][:MsgType] == "event") && (params[:xml][:Event] == "subscribe")
       render "welcome", :formats => :xml
     end
@@ -18,7 +16,7 @@ class WeixinsController < ApplicationController
   private
   # 根据参数校验请求是否合法，如果非法返回错误页面
   def check_weixin_legality
-    array = ["sumersmile", params[:timestamp], params[:nonce]].sort
+    array = [Rails.configuration.weixin_token, params[:timestamp], params[:nonce]].sort
     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module AnnaPatio
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "zh-CN"
+    
+    # 微信 API token
+    config.weixin_token = "o2music"
   end
 end
