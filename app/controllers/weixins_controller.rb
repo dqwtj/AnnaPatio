e1f4134b1d2cef28ee50b521731d20928481f682
Tianju <wtshootj@Tianju-Wangs-MacBook-Air.local>
2013-12-30 21:56:02 +0800

    wechat poem api

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -8,6 +8,12 @@ class WeixinsController < ApplicationController
   end
 
   def create
+    if params[:xml][:MsgType] == "text"
+      case params[:xml][:Content]
+      when "我的内心是满的"
+        render "poem", :formats => :xml
+      end
+    end
     if (params[:xml][:MsgType] == "event") && (params[:xml][:Event] == "subscribe")
       render "welcome", :formats => :xml
     end
